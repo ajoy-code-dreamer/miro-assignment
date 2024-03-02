cea4b67b3286cdd90104575d2baa860af98ae7af
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-03-02 16:53:34 +0600

    added banner and users section design

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// ============= jQuery operation ==============
+$(function(){
+  
+  
+  $('.users__slider').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay:true,
+    arrows:false,
+  });
+    
+
+
+
+
+})
+
+// ============= jQuery operation ==============
