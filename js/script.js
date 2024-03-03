4e074aadfac5b17a4068d6f9e3ae964737621c86
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-03-04 01:58:59 +0600

    banner and user section responsive done

--- js/script.js
+++ js/script.js
@@ -9,6 +9,29 @@ $(function(){
     autoplay:true,
     arrows:false,
     autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
     
 })
