bc63df8b3063659de888a3cac4d51042e76d04b1
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-03-03 22:24:11 +0600

    some js work added

--- js/script.js
+++ js/script.js
@@ -11,10 +11,26 @@ $(function(){
     autoplaySpeed: 3000,
   });
     
-
-
-
-
 })
 
 // ============= jQuery operation ==============
+
+// ============= javascript operation ==============
+// all selector 
+const navMenu = document.querySelector('.nav__menuWrapperMain')
+const collapseClose = document.querySelector('.close__icon i')
+
+const menuIcon = document.querySelector('.collapse__menuIcon i')
+// all selector 
+
+menuIcon.addEventListener('click',()=>{
+  navMenu.classList.toggle('menu__active')
+  menuIcon.classList.toggle('fa-xmark')
+})
+collapseClose.addEventListener('click',()=>{
+  navMenu.classList.toggle('menu__active')
+  menuIcon.classList.toggle('fa-xmark')
+})
+
+
+// ============= javascript operation ==============
